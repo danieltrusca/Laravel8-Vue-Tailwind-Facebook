2cda50c48d0e4fbd2661db259687a54230661300
Dan Trusca <truscadani@yahoo.com>
2021-02-23 12:02:14 +0200

    user posts test

--- routes/api.php
+++ routes/api.php
@@ -4,12 +4,17 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\PostController;
 use App\Http\Controllers\UserController;
-
+use App\Http\Controllers\UserPostController;
 
 
 Route::middleware('auth:api')->group(function(){
-    Route::apiResource('posts', PostController::class);
-    Route::apiResource('users', UserController::class);
+    // Route::apiResource('posts', PostController::class);
+    // Route::apiResource('users', UserController::class);
+    Route::apiResources([
+        'posts'=>PostController::class,
+        'users'=> UserController::class,
+        'users/{user}/posts'=>UserPostController::class
+    ]);
 
 });
 // Route::get('/posts', [PostController::class, 'index']);
