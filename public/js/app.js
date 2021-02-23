77ce128f3a7c0288b0b7c71135cfed69bb30e082
Dan Trusca <truscadani@yahoo.com>
2021-02-23 20:03:01 +0200

    user Profile Design

--- public/js/app.js
+++ public/js/app.js
@@ -2238,6 +2238,7 @@ __webpack_require__.r(__webpack_exports__);
 /* harmony export */ __webpack_require__.d(__webpack_exports__, {
 /* harmony export */   "default": () => (__WEBPACK_DEFAULT_EXPORT__)
 /* harmony export */ });
+/* harmony import */ var _components_Post_vue__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ../../components/Post.vue */ "./resources/js/components/Post.vue");
 //
 //
 //
@@ -2250,13 +2251,42 @@ __webpack_require__.r(__webpack_exports__);
 //
 //
 //
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+
 /* harmony default export */ const __WEBPACK_DEFAULT_EXPORT__ = ({
   name: "Show",
+  components: {
+    Post: _components_Post_vue__WEBPACK_IMPORTED_MODULE_0__.default
+  },
   data: function data() {
     return {
       user: null,
-      loading: true,
-      posts: []
+      userLoading: true,
+      posts: [],
+      postsLoading: true
     };
   },
   mounted: function mounted() {
@@ -2264,17 +2294,17 @@ __webpack_require__.r(__webpack_exports__);
 
     axios.get("/api/users/" + this.$route.params.userId).then(function (res) {
       _this.user = res.data;
-      _this.loading = false;
     })["catch"](function (error) {
       console.log("Unable to fetch the user from the server");
-      _this.loading = false;
+    })["finally"](function () {
+      _this.userLoading = false;
     });
-    axios.get("/api/posts/" + this.$route.params.userId).then(function (res) {
+    axios.get("/api/users/" + this.$route.params.userId + "/posts").then(function (res) {
       _this.posts = res.data;
-      _this.loading = false;
     })["catch"](function (error) {
       console.log(error.message);
-      _this.loading = false;
+    })["finally"](function () {
+      _this.postsLoading = false;
     });
   }
 });
@@ -38891,24 +38921,74 @@ var render = function() {
   var _vm = this
   var _h = _vm.$createElement
   var _c = _vm._self._c || _h
-  return _vm._m(0)
+  return _c(
+    "div",
+    { staticClass: "flex flex-col items-center" },
+    [
+      _c("div", { staticClass: "relative mb-8" }, [
+        _vm._m(0),
+        _vm._v(" "),
+        _c(
+          "div",
+          {
+            staticClass:
+              "absolute flex items-center bottom-0 left-0 -mb-8 ml-12 z-20"
+          },
+          [
+            _vm._m(1),
+            _vm._v(" "),
+            _c("p", { staticClass: "text-2xl text-gray-100 ml-4" }, [
+              _vm._v(
+                "\n                " +
+                  _vm._s(_vm.user ? _vm.user.data.attributes.name : "") +
+                  "\n            "
+              )
+            ])
+          ]
+        )
+      ]),
+      _vm._v(" "),
+      _vm.postsLoading
+        ? _c("p", [_vm._v("Loading...")])
+        : _vm.posts.length < 1
+        ? _c("div", [_vm._v("No posts found. Get started...")])
+        : _vm._l(_vm.posts.data, function(post) {
+            return _c("Post", { key: post.data.post_id, attrs: { post: post } })
+          })
+    ],
+    2
+  )
 }
 var staticRenderFns = [
   function() {
     var _vm = this
     var _h = _vm.$createElement
     var _c = _vm._self._c || _h
-    return _c("div", [
-      _c("div", { staticClass: "w-100 h-64 overflow-hidden z-10" }, [
-        _c("img", {
-          staticClass: "object-cover w-full",
-          attrs: {
-            src:
-              "https://images.pexels.com/photos/719597/pexels-photo-719597.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
-            alt: "user_background"
-          }
-        })
-      ])
+    return _c("div", { staticClass: "w-100 h-64 overflow-hidden z-10" }, [
+      _c("img", {
+        staticClass: "object-cover w-full",
+        attrs: {
+          src:
+            "https://images.pexels.com/photos/719597/pexels-photo-719597.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
+          alt: "user_background"
+        }
+      })
+    ])
+  },
+  function() {
+    var _vm = this
+    var _h = _vm.$createElement
+    var _c = _vm._self._c || _h
+    return _c("div", { staticClass: "w-32" }, [
+      _c("img", {
+        staticClass:
+          "object-cover w-32 h-32 border-4 border-gray-200 rounded-full shadow-lg",
+        attrs: {
+          src:
+            "https://images.pexels.com/photos/670720/pexels-photo-670720.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500",
+          alt: "user_profile_image"
+        }
+      })
     ])
   }
 ]
--- resources/js/views/users/Show.vue
+++ resources/js/views/users/Show.vue
@@ -1,24 +1,52 @@
 <template>
-    <div>
-        <div class="w-100 h-64 overflow-hidden z-10">
-            <img
-                src="https://images.pexels.com/photos/719597/pexels-photo-719597.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940"
-                alt="user_background"
-                class="object-cover w-full"
-            />
+    <div class="flex flex-col items-center">
+        <div class="relative mb-8">
+            <div class="w-100 h-64 overflow-hidden z-10">
+                <img
+                    src="https://images.pexels.com/photos/719597/pexels-photo-719597.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940"
+                    alt="user_background"
+                    class="object-cover w-full"
+                />
+            </div>
+            <div
+                class="absolute flex items-center bottom-0 left-0 -mb-8 ml-12 z-20"
+            >
+                <div class="w-32">
+                    <img
+                        src="https://images.pexels.com/photos/670720/pexels-photo-670720.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500"
+                        alt="user_profile_image"
+                        class="object-cover w-32 h-32 border-4 border-gray-200 rounded-full shadow-lg"
+                    />
+                </div>
+                <p class="text-2xl text-gray-100 ml-4">
+                    {{ user ? user.data.attributes.name : "" }}
+                </p>
+            </div>
         </div>
+        <p v-if="postsLoading">Loading...</p>
+        <div v-else-if="posts.length < 1">No posts found. Get started...</div>
+        <Post
+            v-else
+            v-for="post in posts.data"
+            :key="post.data.post_id"
+            :post="post"
+        />
     </div>
 </template>
 
 <script>
+import Post from "../../components/Post.vue";
 export default {
     name: "Show",
-
+    components: {
+        Post
+    },
     data: () => {
         return {
             user: null,
-            loading: true,
-            posts: []
+            userLoading: true,
+            posts: [],
+            postsLoading: true
         };
     },
 
@@ -27,22 +55,24 @@ export default {
             .get("/api/users/" + this.$route.params.userId)
             .then(res => {
                 this.user = res.data;
-                this.loading = false;
             })
             .catch(error => {
                 console.log("Unable to fetch the user from the server");
-                this.loading = false;
+            })
+            .finally(() => {
+                this.userLoading = false;
             });
 
         axios
-            .get("/api/posts/" + this.$route.params.userId)
+            .get("/api/users/" + this.$route.params.userId + "/posts")
             .then(res => {
                 this.posts = res.data;
-                this.loading = false;
             })
             .catch(error => {
                 console.log(error.message);
-                this.loading = false;
+            })
+            .finally(() => {
+                this.postsLoading = false;
             });
     }
 };
