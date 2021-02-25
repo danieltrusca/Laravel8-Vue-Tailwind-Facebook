1177e11c3f552305bd9db7c1ee4fa0993e49848c
Dan Trusca <truscadani@yahoo.com>
2021-02-25 16:42:19 +0200

    locking down the API

--- app/Http/Controllers/FriendRequestResponseController.php
+++ app/Http/Controllers/FriendRequestResponseController.php
@@ -5,6 +5,8 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\Models\Friend;
 use App\Http\Resources\FriendResource;
+use App\Exceptions\FriendRequestNotFoundException;
+use Illuminate\Database\Eloquent\ModelNotFoundException;
 
 class FriendRequestResponseController extends Controller
 {
@@ -15,9 +17,15 @@ class FriendRequestResponseController extends Controller
             'status'=>''
         ]);
 
-        $friendRequest=Friend::where('user_id', $data['user_id'])
+        try {
+            $friendRequest=Friend::where('user_id', $data['user_id'])
                 ->where('friend_id', auth()->user()->id)
                 ->firstOrFail();
+        } catch (ModelNotFoundException $e) {
+            throw new FriendRequestNotFoundException();
+        }
+
+
 
         $friendRequest->update(array_merge($data, [
             'confirmed_at'=>now()
