4e6afcc4481e635432d395aeceb4add73e136d29
Dan Trusca <truscadani@yahoo.com>
2021-02-27 13:18:26 +0200

    user can like posts test

---
+++ tests/Feature/LikesTest.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace Tests\Feature;
+
+use App\Models\Post;
+use App\Models\User;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Tests\TestCase;
+
+   //$this->withoutExceptionHandling();
+    // $this->actingAs($user = User::factory()->create(), 'api');
+    // $anotherUser=User::factory()->create();
+
+    // $posts = Post::factory()->count(2)->create(['user_id'=>$anotherUser->id]);
+
+class LikesTest extends TestCase
+{
+    use RefreshDatabase;
+
+    /** @test */
+    public function a_user_can_like_a_post()
+    {
+        $this->withoutExceptionHandling();
+
+        $this->actingAs($user = User::factory()->create(), 'api');
+
+        $post = Post::factory()->create(['id'=>123]);
+
+        $response = $this->post('/api/posts/'.$post->id.'/like')
+            ->assertStatus(200);
+
+        $this->assertCount(1, $user->likedPosts);
+
+        $response->assertJson([
+            'data' => [
+                [
+                    'data' => [
+                        'type' => 'likes',
+                        'like_id' => 1,
+                        'attributes' => []
+                    ],
+                    'links' => [
+                        'self' => url('/posts/123'),
+                    ]
+                ]
+            ],
+            'links' => [
+                'self' => url('/posts'),
+            ]
+        ]);
+    }
+}
